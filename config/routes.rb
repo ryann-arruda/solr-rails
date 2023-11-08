8b5d02062d45693fb758f92a5dfe3613645ca959
Ryann <ryann.arruda_159@hotmail.com>
2023-11-08 10:38:37 -0300

    Adição da rota de pesquisa

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@
 
 Rails.application.routes.draw do
   resources :users
+  #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    collection do
+      post :search  # creates a route called users_search
+    end
+  end
+
+end
 end
