8ff9444a3211c4e3d285961a6520108896e36732
Ryann Carlos de Arruda Quintino <53544629+ryann-arruda@users.noreply.github.com>
2023-11-08 11:26:56 -0300

    Atualização de routes.rb
    Remoção de rota conflituosa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
   #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
